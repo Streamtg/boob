fce89203a8e8b28e3d42d64346632f714dcf3508
Streamtg <202310475@issu.edu.do>
2025-08-20 14:23:30 -0400

    Update start.go

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -1,1 +1,32 @@
+package commands
 
+import (
+	"EverythingSuckz/fsb/config"
+	"EverythingSuckz/fsb/internal/utils"
+
+	"github.com/celestix/gotgproto/dispatcher"
+	"github.com/celestix/gotgproto/dispatcher/handlers"
+	"github.com/celestix/gotgproto/ext"
+	"github.com/celestix/gotgproto/storage"
+)
+
+func (m *command) LoadStart(dispatcher dispatcher.Dispatcher) {
+	log := m.log.Named("start")
+	defer log.Sugar().Info("Loaded")
+	dispatcher.AddHandler(handlers.NewCommand("start", start))
+}
+
+func start(ctx *ext.Context, u *ext.Update) error {
+	chatId := u.EffectiveChat().GetID()
+	peerChatId := ctx.PeerStorage.GetPeerById(chatId)
+	if peerChatId.Type != int(storage.TypeUser) {
+		return dispatcher.EndGroups
+	}
+	if len(config.ValueOf.AllowedUsers) != 0 && !utils.Contains(config.ValueOf.AllowedUsers, chatId) {
+		ctx.Reply(u, "You are not allowed to use this bot.", nil)
+		return dispatcher.EndGroups
+	}
+
+	ctx.Reply(u, "Need a direct streamable link to a file? Send it my way! 🤓\n\nJoin my Update Channel @yoelbots 🗿 for more updates.\n\nLink validity: 24 hours ⏳\n\nPro Tip: Use 1DM Browser for lightning-fast downloads! 🔥\n\n📊 Use /stats to view bot statistics", nil)
+	return dispatcher.EndGroups
+}
