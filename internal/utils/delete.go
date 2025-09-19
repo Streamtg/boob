8e7fdc8f1278c783c9abb10c7c9248f4faf4b081
Streamtg <202310475@issu.edu.do>
2025-09-19 09:49:56 -0400

    Update delete.go

--- internal/utils/delete.go
+++ internal/utils/delete.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gotd/td/tg"
 )
 
-// DeleteMessage elimina un mensaje de un canal específico (e.g., original en LOG_CHANNEL)
+// DeleteMessage elimina un mensaje de un canal específico
 func DeleteMessage(ctx *ext.Context, channelID int64, messageID int) error {
 	_, err := ctx.Raw.ChannelsDeleteMessages(&tg.ChannelsDeleteMessagesRequest{
 		Channel: &tg.InputChannel{ChannelID: channelID},
