1282e9fcfebd212245c28985feeb503dc20018d2
Streamtg <202310475@issu.edu.do>
2025-10-04 17:21:40 -0400

    Update file.go

--- internal/types/file.go
+++ internal/types/file.go
@@ -9,21 +9,26 @@ import (
 	"github.com/gotd/td/tg"
 )
 
+// File representa un archivo en Telegram, ya sea documento, foto, audio o video.
 type File struct {
 	Location tg.InputFileLocationClass
 	FileSize int64
 	FileName string
 	MimeType string
 	ID       int64
+	Duration int64 // Nueva propiedad: duración en segundos para audio/video
 }
 
+// HashableFileStruct contiene los campos que se usarán para generar un hash único
 type HashableFileStruct struct {
 	FileName string
 	FileSize int64
 	MimeType string
 	FileID   int64
+	Duration int64 // Incluimos duración en el hash
 }
 
+// Pack genera un hash MD5 único del archivo
 func (f *HashableFileStruct) Pack() string {
 	hasher := md5.New()
 	val := reflect.ValueOf(*f)
@@ -41,4 +46,15 @@ func (f *HashableFileStruct) Pack() string {
 		hasher.Write(fieldValue)
 	}
 	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+// Nuevo helper que crea un HashableFileStruct desde un File
+func (f *File) ToHashable() *HashableFileStruct {
+	return &HashableFileStruct{
+		FileName: f.FileName,
+		FileSize: f.FileSize,
+		MimeType: f.MimeType,
+		FileID:   f.ID,
+		Duration: f.Duration, // Incluimos duración
+	}
 }
