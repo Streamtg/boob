4e10ddcb87e1a6c92b9ba05d3e1b89be73a87a00
Streamtg <202310475@issu.edu.do>
2025-12-22 22:59:20 -0400

    Update commands.go

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -7,16 +7,29 @@ import (
 	"go.uber.org/zap"
 )
 
+// Definimos la estructura una sola vez para todo el paquete
 type command struct {
 	log *zap.Logger
 }
 
+// Load inicializa todos los handlers del paquete commands
 func Load(log *zap.Logger, dispatcher dispatcher.Dispatcher) {
 	log = log.Named("commands")
-	defer log.Info("Initialized all command handlers")
-	Type := reflect.TypeOf(&command{log})
-	Value := reflect.ValueOf(&command{log})
+	defer log.Info("All command handlers have been initialized")
+
+	// Instanciamos la estructura con el logger
+	cmdInstance := &command{log: log}
+	
+	Type := reflect.TypeOf(cmdInstance)
+	Value := reflect.ValueOf(cmdInstance)
+
+	// Iteramos sobre todos los métodos vinculados a la estructura 'command'
 	for i := 0; i < Type.NumMethod(); i++ {
-		Type.Method(i).Func.Call([]reflect.Value{Value, reflect.ValueOf(dispatcher)})
+		method := Type.Method(i)
+		// Saltamos el cargador para no entrar en recursión si tuviera la misma firma
+		if method.Name == "Load" {
+			continue
+		}
+		method.Func.Call([]reflect.Value{Value, reflect.ValueOf(dispatcher)})
 	}
 }
